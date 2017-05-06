0c67f27169fde883059ac8222b181952225f5b54
Jason Liu <jasobs10@gmail.com>
2017-05-06 16:55:19 -0400

    Add styling and additional fetches

--- demo/thundernews.js
+++ demo/thundernews.js
@@ -1,12 +1,13 @@
 $td(() => {
-
+  const tech = $td('.tech');
+  const sports = $td('.sports');
+  const news = $td('.newsweek');
   const imgHeader = $td('.article-img');
-  const articleImg = $td('.article-img');
+
   const title = $td('.title');
   const author = $td('.author');
   const description = $td('.description');
   const urlLink = $td('.url');
-
 
   const fetch = (source) => {
     const apiKey="bdac8277aae0437f96fbed63f7b07a00";
@@ -19,14 +20,56 @@ $td(() => {
       console.log(r);
       const article = r.articles[0];
       console.log(article.urlToImage);
-      imgHeader.append(`<img src=${article.urlToImage}/>`);
+      imgHeader.append(`<img src=${article.urlToImage}>`);
       title.html(`${article.title}`);
-      author.html(`By ${article.author}`);
-      description.html(article.description);
-      urlLink.html(article.url);
+      author.html(`${article.author}`);
+      description.html(article.description + ` <a href=${article.url}>full article</a>`);
+      // urlLink.append(`<a href=${article.url}>Read Full Article</a>`);
 
     });
+
+    switch(source) {
+      case 'techcrunch':
+        tech.addClass("current");
+        sports.removeClass("current");
+        news.removeClass('current');
+        break;
+      case 'fox-sports':
+        sports.addClass('current');
+        tech.removeClass('current');
+        news.removeClass('current');
+        break;
+      case 'newsweek':
+        news.addClass('current');
+        tech.removeClass('current');
+        sports.removeClass('current');
+        break;
+      default:
+        break;
+
+    }
   };
+
+  const clear = () => {
+    title.empty();
+    author.empty();
+    description.empty();
+    imgHeader.empty();
+
+  };
+
+  tech.on('click',  () => {
+    clear();
+    fetch('techcrunch');
+  });
+  sports.on('click', () => {
+    clear();
+    fetch('fox-sports');
+  });
+  news.on('click', () => {
+    clear();
+    fetch('newsweek');
+  });
 
   fetch('techcrunch');
 });
